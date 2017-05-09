63e4413c3122c5354f5824f6dbf755813caaae56
Harold Clark <harold.clark@openscg.com>
2017-05-09 13:12:13 -0400

    remove no asysnc server.py, renamed server_uasyncio.py to servers.py, modified __init__.py for these chnages

--- WaterPumps/__init__.py
+++ WaterPumps/__init__.py
@@ -9,7 +9,6 @@ from WaterPumps.flowMeters import flowRunData
 from WaterPumps.leds import led
 from WaterPumps.leds import triLed
 from WaterPumps.pressure import pressureSensor
-from WaterPumps.server_uasyncio import pumpServer
-from WaterPumps.server_uasyncio import validCommand
+from WaterPumps.servers import pumpServer
+from WaterPumps.servers import validCommand
 from WaterPumps.events import Event
-
--- WaterPumps/servers.py
+++ WaterPumps/servers.py
@@ -1,115 +1,120 @@
 # Author: Harold Clark
 # Copyright Harold Clark 2017
 #
-import network
-import time
-import socket
-import sys
-import uasyncio.core as asyncio
+try:
+    import lib.uasyncio.core as asyncio
+except ImportError:
+    import uasyncio.core as asyncio
+from WaterPumps.events import Event
 
 class pumpServer(object):
-    def __init__(self, port=8888, host='', connectionCount=5, validCommandList=['PumpOn']):
-        """init pump server"""
-        self.port = port
+    """Class for pumpserver using uasyncio"""
+    def __init__(self, host='', port=8888):
+        """initilzed the pump server class """
+        import machine
+        self.serverName = 'Test Server'
         self.host = host
-        self.connectionCount = connectionCount
-        self.socket = None
-        self.validCommandList = validCommandList
-        self.ver = '0.9'
+        self.port = port
+        self.validCommands = []
 
 
-    def pumpServerStart(self):  
-        """Server for pump objects"""
-        # self.HOST  - Symbolic name meaning all available interfaces
-        # self.PORT - Arbitrary non-privileged port
- 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print('Socket created')
-        self.socket.bind(('192.168.1.14',8888))
-         
-        print('Socket bind complete')
-     
-        self.socket.listen(self.connectionCount)
-        print('Socket now listening')
-        
-    async def listenForConnection(self):
-        #wait to accept a connection - blocking call
-        c = pumpconnection(self.validCommandList)
-        
-        c.conn, c.addr = self.socket.socket.accept()     
-        print("""Connected with %s:%s""" % (c.addr[0], str(c.addr[1])))
-        await asyncio.sleep_ms(50)
-        #now keep talking with the client
-        while c.cleanData=='Exit':
-            await asyncio.sleep_ms(50)
-            c.data = str(c.conn.recv(512))
-            c.clean(c.data)
-            c.checkMessage(debug=True)        
-            c.conn.sendall(c.message)
-            print(c.message)
-        c.conn.close()
-        #self.socket.close()
-        await asyncio.sleep_ms(50)
-    
-    
+    def validCommandList(self):
+        """Method for extrating list for vaild commands"""
+        list = []
+        for v in self.validCommands:
+            list.append(v.name)
+        return list    
+            
     def clearvalidCommandList(self):
         """Clear the CommandList to prep for reload of list"""
-        self.validCommandList = []
+        self.validCommands = []
     
     
     def appendvalidCommandlist(self, commands):
-        """ Add new memebers the validCommandList """
+        """Add new memebers the validCommandList """
         if type(commands) is list:
             for i in commands:        
-                self.validCommandList.append(i)
+                self.validCommands.append(i)
         else:
-            self.validCommandList.append(commands)
+            self.validCommands.append(commands)
     
     def setvalidCommandList(self, commands):
         """Set validCommandlist to list of supplied commands"""
         self.clearvalidCommandList()
         self.appendvalidCommandlist(commands)
                 
-        
-class pumpconnection(object):
-    """Object for holding connection data"""
-    def __init__(self, validCommandList):
-        """init object with conn, addr"""
-        self.conn = None
-        self.addr = None 
-        self.data = None
-        self.cleanData = None
-        self.message = None
-        self.validCommandList = validCommandList
+    def addTasktoLoop(self, command, event, debug=True):
+        """add a func to main loop"""
+        msg = "bad no coro"
+        for c in self.validCommands:
+            if c.name==command:        
+                pservertask = c.commandCoro(event)
+                mainLoop = asyncio.get_event_loop()
+                if debug:
+                    print("""Loaded task: %s as handle %s""" % (c.name, pservertask))
+                mainLoop.create_task(pservertask)
+                msg = c.commandCoro
+                return msg                
+        return msg
     
-    def clean(self, data):
-        """Cleaned data from client"""
-        self.cleanData =  str(data)[2:-5]
-        #colon = instr(cleanData)
-        # not sure about this line HEC
-        #self.cleanData = str(self.cleanData)
+    
+    def pserverHelp(self):
+        msg = """Welcome to %s\n\r\n\rip: %s on port %s\n\rexit - Exit session\n\rlist - list avalible commands\n\r
+help - this info\n\r""" % (self.serverName,
+                        self.host,
+                        self.port)
+        return msg
+    
+    
+    def pserverList(self):
+        msg = """List of commands\n\r"""
+        for c in self.validCommandList():
+            msg = msg +  c + '\n\r'
+        return msg
+    
+    def telnetPrint(self, msg):
+        """function for converting message from moduole to telnet happy strings"""
+        msg = """%s\n\r""" % (msg)
+        return msg
+    
+    
+    @asyncio.coroutine
+    def pserver(self, reader, writer):
+        """coroutine for reading server requests"""
+        print(reader, writer)
+        print("================")
+        CommandDataEvent = Event()
+        while True:
+            command = yield from reader.read()
+            command = str(command)[2:-5]
+            msg = ''
+            if command in self.validCommandList():
+                self.addTasktoLoop(command, CommandDataEvent)
+                await CommandDataEvent
+                msg = self.telnetPrint(CommandDataEvent.value())
+                CommandDataEvent.clear()
+                
+            elif command=='help':
+                msg = self.pserverHelp()
+            elif command=='list':
+                msg = self.pserverList()
+            elif command in ['exit','quit']:
+                msg = "bye\n\r"
+            else:
+                msg = """Invalid Code - %s\n\r""" % (command)
+            print(msg)
+            yield from writer.awrite(msg)
+            print("After response write")
+            if command=='exit':
+                break
+        yield from writer.aclose()
+        print("Finished processing request")  
+    
+class validCommand(object):
+    """Class for a Vaild Command on server"""
+    def __init__(self,name='',commandCoro=None,commandArgs=None):
+        """initilized vaildCommand"""
+        self.name = name
+        self.commandCoro = commandCoro
+        self.commandArgs = commandArgs
 
-    
-    def NewData(self, data):
-        """add data, and cleandata to object"""
-        self.data = data
-        self.clean()
-    
-    
-    def checkMessage(self, debug=False):
-        
-        if self.cleanData in self.validCommandList:
-            self.message = "Valid command"
-        else:
-            if debug:
-                print("""cleandata: %s        data: %s""" % (self.cleanData,self.data))
-                print("""validCommandList count: %s     validCommandList: %s""" % (len(self.validCommandList),self.validCommandList))
-            self.message = 'exit'
-            self.cleanData = 'exit'
-        self.message = """%s\r\n""" % (self.message)    
-    
-
-class pumpserverfuture(object):
-    def __init__(self):
-        self.result = None
-        
