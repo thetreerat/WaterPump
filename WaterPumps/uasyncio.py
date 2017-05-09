f3a4ed86f99f963c9fe00f112098ae6f2725ce0f
Harold Clark <harold.clark@openscg.com>
2017-05-09 01:42:50 -0400

    remove dead file uasyncio.py

--- WaterPumps/uasyncio.py
+++
@@ -1,7 +0,0 @@
-from os import listdir        
-import upip
-if not 'lib' in listdir():
-    upip.install('micropython-uasyncio.core')
-from os import remove
-remove('main.py')
-print('finished, reboot controller')
