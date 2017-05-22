af19219a8a9045226f0618a127008285ac507c05
Harold Clark <harold.clark@openscg.com>
2017-05-21 22:41:39 -0400

    finsish point for the night

--- WaterPumps/controllers.py
+++ WaterPumps/controllers.py
@@ -24,7 +24,7 @@ class controller(object):
         self.launchEvent = Event(name='''%s - Lauch Event''' % (name))
         self.launchEvent.set(time())
         self.launchCount = 0
-    
+        self.controllerClose = Event()
     
     def name(self):
         return self._name
@@ -50,22 +50,22 @@ class controller(object):
                 self.OSErrorEvent.set(e.args[0])
                 raise
         print('''%s - %s: connected to %s on %s''' % (self.name(), time(), self.ip, self.port))
-        while True:
+        while not self.controllerCloseEvent.is_set():
             await asyncio.sleep_ms(500)
             if len(self.messageQue):
                  command, event = self.messageQue.pop()
                  try:
                     s.send(command)
-                 except OSError as e:
+                 except socket.error as e:
                     print(e)
                     s.close()
-                    self.activeEvent.clear()
-                    raise
-                 controllerMessage = s.recv(512).decode("utf-8")[:-2]
+                    self.notActiveEvent.set(time() + 7)
+                    self.launchEvent.set(time())
+                 controllerMessage = s.recv().decode("utf-8")[:-2]
+                 #cleanMessage = controllerMessage
                  event.set(controllerMessage)
                  if debug:
                     print('''%s - %s: return message: %s''' % (self.name(), time(),controllerMessage))
         s.send('exit\r\n')
         s.close()
         self.notActiveEvent.set(time() + 10)
-        
