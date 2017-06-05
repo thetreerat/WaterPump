5ab19640d3f0a4d902b7051c40f15cc5ebd1e5b2
Harold Clark <harold.clark@openscg.com>
2017-06-05 11:02:34 -0400

    saving work

--- ExampleValve/main.py
+++ ExampleValve/main.py
@@ -11,21 +11,26 @@ try:
     import logging
 except ImportError:
     import lib.logging as logging
-    
+
+from WaterPumps.events import Event
 from WaterPumps.valves import valve
 from WaterPumps.leds import led
 from WaterPumps.buttons import button
 from WaterPumps.buttons import state
 from WaterPumps.servers import pumpServer
 from WaterPumps.validCommands import validCommand
+from valvefuncs import addWater
 
 import network
 logging.basicConfig(level=logging.DEBUG)
 # define valve
 pondValve = valve(15, 'Pond Valve')
+addWaterEvent = Event(name='Add Water Event')
+pondValue.registerMonitorEvent('addWater25', addWaterEvent, addWater)
 # define fill button
+
 fillButton = button(5, name='Fill Button')
-fillStates = [state('Add 25 gallons', event=valveFunc.AddEvent)]
+fillStates = [state('Add 25 gallons', event=addWaterEvent)]
 fillButton.states.setStates(fillStates)
 #define cancel Button
 cancelButton = button(4, name='Cancel Button')
--- ExampleValve/valvefuncs.py
+++ ExampleValve/valvefuncs.py
@@ -1,3 +1,10 @@
 # Author: Harold Clark
 # Copyright Harold Clark 2017
 #
+try:
+    import lib.uasyncio as asyncio
+except ImportError:
+    import uasyncio as asyncio
+
+async def addWater(litters=94):
+    pass
